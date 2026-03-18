8565b33287f545a46d8527acaecf329f73335e7c
VELİ DOĞAN <velidogan120@gmail.com>
2026-03-18 10:27:37 +0300

    feat: added initial project setup with requirements

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
