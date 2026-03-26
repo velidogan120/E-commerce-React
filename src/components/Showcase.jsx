bb42d24ccd1f94ebf74f9f10a33c3e1c39e24b72
VELİ DOĞAN <velidogan120@gmail.com>
2026-03-26 20:57:50 +0300

    Added Showcase component and styles

---
+++ src/components/Showcase.jsx
@@ -0,0 +1,18 @@
+import "../styles/showcase.css";
+const Showcase = () => {
+  return (
+    <div className="showcase">
+      <div className="showcase-left">
+        <img src="/showcase/showcase-1.jpg" alt="showcase" />
+      </div>
+      <div className="showcase-right">
+        <img src="/showcase/showcase-2.jpg" alt="showcase" />
+        <img src="/showcase/showcase-3.jpg" alt="showcase" />
+        <img src="/showcase/showcase-4.jpg" alt="showcase" />
+        <img src="/showcase/showcase-5.jpg" alt="showcase" />
+      </div>
+    </div>
+  );
+};
+
+export default Showcase;
--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,10 +1,12 @@
 import Breadcrumb from "../components/shared/Breadcrumb";
 import Trial from "../components/shared/Trial";
+import Showcase from "../components/Showcase";
 import TeamEmployee from "../components/TeamEmployee";
 const TeamPage = () => {
   return (
     <>
       <Breadcrumb />
+      <Showcase />
       <TeamEmployee />
       <Trial />
     </>
